9886118d9c2896062869ea90f374c9cf36593d2e
tbrunain <t.brunain@protonmail.com>
2021-03-20 22:50:57 +0100

    feat: replace web3 call by covalent api call + add number of token holders

--- src/BurnedTokens.js
+++ src/BurnedTokens.js
@@ -2,8 +2,7 @@
 import './BurnedTokens.css'
 
 import React from 'react'
-import Web3 from 'web3'
-import {sporeABI} from "./utils/SporeAbi";
+import axios from "axios";
 
 const TOTAL_SUPPLY = 100000000000000000;
 
@@ -14,6 +13,7 @@ class BurnedTokens extends React.Component {
         this.state = {
             numberOfBurnedTokens: -1,
             percentageOfBurnedTokens: -1,
+            numberOfTokenHolders: -1,
         };
     }
 
@@ -26,12 +26,16 @@ class BurnedTokens extends React.Component {
                 <li>
                     % of Burned Tokens : <b>{this.state.percentageOfBurnedTokens}</b> %
                 </li>
+                <li>
+                    Number of token holders : <b>{this.state.numberOfTokenHolders}</b>
+                </li>
             </React.Fragment>
         )
     }
 
     async componentDidMount() {
         await this.getBurnedTokens()
+        await this.getTokenHolders()
         // Will trigger a refresh every minutes
         setTimeout(async () => {
             await this.getBurnedTokens();
@@ -39,14 +43,40 @@ class BurnedTokens extends React.Component {
     }
 
     async getBurnedTokens() {
-        window.web3 = new Web3(window.ethereum);
-        window.ethereum.enable();
-        const sporeERC20 = new window.web3.eth.Contract(sporeABI, "0x6e7f5C0b9f4432716bDd0a77a3601291b9D9e985")
-        const burnedTokens = await sporeERC20.methods.balanceOf("0x000000000000000000000000000000000000dEaD").call()
-        this.setState({
-            numberOfBurnedTokens: this.numberWithCommas((burnedTokens / 10 ** 9).toFixed(0)),
-            percentageOfBurnedTokens: ((burnedTokens / 10 ** 9) / TOTAL_SUPPLY * 100).toFixed(2)
-        })
+        // window.web3 = new Web3(window.ethereum);
+        // window.ethereum.enable();
+        const res = await axios.get(
+            'https://api.covalenthq.com/v1/43114/address/0x000000000000000000000000000000000000dEaD/balances_v2/?nft=false'
+        );
+
+        if (res.data !== undefined && res.data.data !== undefined && res.data.data.items !== undefined) {
+            const items = res.data.data.items;
+            items.forEach((coin) => {
+                if (coin.contract_address === '0x6e7f5c0b9f4432716bdd0a77a3601291b9d9e985') {
+                    this.setState({
+                        numberOfBurnedTokens: this.numberWithCommas((coin.balance / 10 ** 9).toFixed(0)),
+                        percentageOfBurnedTokens: ((coin.balance / 10 ** 9) / TOTAL_SUPPLY * 100).toFixed(2)
+                    })
+                }
+            })
+        }
+    }
+
+    async getTokenHolders() {
+        // window.web3 = new Web3(window.ethereum);
+        // window.ethereum.enable();
+        const res = await axios.get(
+            'https://api.covalenthq.com/v1/43114/tokens/0x6e7f5C0b9f4432716bDd0a77a3601291b9D9e985/token_holders/?page-size=999999'
+        );
+
+        console.log(res)
+
+        if (res.data !== undefined && res.data.data !== undefined && res.data.data.items !== undefined) {
+            const items = res.data.data.items;
+            this.setState({
+                numberOfTokenHolders: items.length
+            })
+        }
     }
 
     numberWithCommas(x) {
