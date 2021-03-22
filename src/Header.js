3890bd9639fcc992d11f1607aed1fbb9b05cccaf
Louis <larotu00@gmail.com>
2021-03-22 18:06:12 +0100

    fix for airdrop button

--- src/Header.js
+++ src/Header.js
@@ -3,35 +3,28 @@ import './Header.css';
 
 import React from 'react'
 import Web3 from 'web3'
+import { findAllInRenderedTree } from 'react-dom/test-utils';
 
 function connectMetaMask() {
   if (window.ethereum) {
-      window.web3 = new Web3(window.currentProvider);
+      window.web3 = new Web3(window.ethereum);
       window.ethereum.enable();
       console.log(window.web3.version)
   }
 }
 
-function claimAirdrop(e){
+async function claimAirdrop(e){
   e.preventDefault();
+  connectMetaMask();
   var ABI = [{"type":"constructor","stateMutability":"nonpayable","inputs":[{"type":"address","name":"contrak","internalType":"contract IERC20"}]},{"type":"event","name":"Deposit","inputs":[{"type":"address","name":"user","internalType":"address","indexed":true},{"type":"uint256","name":"pid","internalType":"uint256","indexed":false},{"type":"uint256","name":"amount","internalType":"uint256","indexed":false}],"anonymous":false},{"type":"event","name":"OwnershipTransferred","inputs":[{"type":"address","name":"previousOwner","internalType":"address","indexed":true},{"type":"address","name":"newOwner","internalType":"address","indexed":true}],"anonymous":false},{"type":"event","name":"Withdraw","inputs":[{"type":"address","name":"user","internalType":"address","indexed":true},{"type":"uint256","name":"pid","internalType":"uint256","indexed":false},{"type":"uint256","name":"amount","internalType":"uint256","indexed":false}],"anonymous":false},{"type":"function","stateMutability":"view","outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"balance","inputs":[]},{"type":"function","stateMutability":"view","outputs":[{"type":"address","name":"","internalType":"contract IERC20"}],"name":"contractaddress","inputs":[]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"deposit","inputs":[{"type":"uint256","name":"_amount","internalType":"uint256"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"get","inputs":[]},{"type":"function","stateMutability":"view","outputs":[{"type":"address","name":"","internalType":"address"}],"name":"owner","inputs":[]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"renounceOwnership","inputs":[]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"transferOwnership","inputs":[{"type":"address","name":"newOwner","internalType":"address"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"update","inputs":[{"type":"uint256","name":"_amount","internalType":"uint256"}]}];
-  var contractAddress = "0xeAdf7D005596dbad55e067C1208080f83258D452";
-  if (window.web3) {
-    window.fairyContract = window.web3.eth.contract(ABI).at(contractAddress)
-    window.fairyContract.get.call({"from": window.web3.eth.accounts[0], "gasPrice":470000000000, "gas":200000}, function(err) {
-        if (!err){
-          window.fairyContract.get.sendTransaction({"from": window.web3.eth.accounts[0], "gasPrice":470000000000, "gas":200000}, function(err, transactionHash) {
-            if (!err){
-              console.log(transactionHash)
-            }
-          })
-        }else{
-          alert("You're already infected (only wallet with 0 spores can claim Airdrop)")
-        }
-      })
-  }else{
-      connectMetaMask()
-      claimAirdrop()
+  const fairyContract = new window.web3.eth.Contract(ABI, "0xeAdf7D005596dbad55e067C1208080f83258D452")
+  var account = await window.web3.eth.getAccounts()
+  account = account[0]
+  console.log(account)
+  try {
+    await fairyContract.methods.get().call({"from":account,"gasPrice":470000000000, "gas":200000})
+  }catch (error){
+    alert(error)
   }
 }
 
