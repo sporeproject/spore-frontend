1acc1119587fa4bf4a02351d38c472ec84b305ac
Kevin McIntyre <kebin70@gmail.com>
2021-04-29 00:08:41 +0000

    l/g

--- src/components/PriceToken.tsx
+++ src/components/PriceToken.tsx
@@ -4,10 +4,8 @@ import axios from 'axios';
 import { useEffect } from 'react';
 import { useState } from 'react';
 
-
-
 const PriceToken = () => {
-  const [priceToken, setPriceToken] = useState({ price: 0, zeros: 0})
+  const [priceToken, setPriceToken] = useState({ avax: 0, zeros: 0, bsc: 0})
   useEffect(() => {
     async function getInfos() {
       await getPriceToken()      
@@ -19,18 +17,15 @@ const PriceToken = () => {
   }, [])
 
   const getPriceToken = async () => {
-    const res = await axios.get('https://x-api.snowball.network/spore/price');
-    if ( res.data !== undefined ) {
-      let ps = res.data.toFixed(18)
-      console.log(ps)
+    const res2 = await axios.get('https://x-api.snowball.network/spore/price');
+    if ( res2.data !== undefined ) {
+      let ps = res2.data.toFixed(18)
       let suffix = ps.split('.')[1]
-      console.log(suffix)
       let i = 0;
       while ( suffix.substring(i, i + 1) === '0' ) {
-        console.log('got here:', i, suffix.substring(i, 1))
         i += 1;
       }      
-      setPriceToken({ price: res.data.toFixed(18), zeros: i})
+      setPriceToken({ avax: res2.data.toFixed(18), zeros: i, bsc: 0 })
     }
   }
 
@@ -41,9 +36,9 @@ const PriceToken = () => {
           target='_blank'
           rel='noopener noreferrer'
           className='btn btn-primary mx-1 mb-2 larger' 
-          href="https://info.pangolin.exchange/#/token/0x6e7f5c0b9f4432716bdd0a77a3601291b9d9e985">
+          href="https://app.pangolin.exchange/#/swap?inputCurrency=0x6e7f5c0b9f4432716bdd0a77a3601291b9d9e985">
           <img height="15px" width="15px" src="avalanche-logo.png" alt="Avalanche Network"></img>
-          💲{priceToken.price}
+          💲{priceToken.avax}
         </a>
       </span>      
       <span className="price larger">        
