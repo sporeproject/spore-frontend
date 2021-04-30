41bb92d1e8e4b6c157a46e01fb471a38adf34ba8
oscar <oscar_blazejewski@yahoo.fr>
2021-04-30 14:55:59 +0200

    Fix key props in contributors

--- src/components/contributors.tsx
+++ src/components/contributors.tsx
@@ -7,15 +7,15 @@ const Contributors = () => {
 
   useEffect(() => {
     async function getContributors() {
-       getContributorsJson()
+      getContributorsJson()
     }
     getContributors()
   }, [])
-  
+
   const getContributorsJson = () => {
-     axios.get(
+    axios.get(
       'https://api.github.com/repos/SporeFinance/Spore-frontend/contributors'
-    ).then( res =>{
+    ).then(res => {
       setContributors(res.data);
     });
   }
@@ -24,18 +24,18 @@ const Contributors = () => {
     <>
       <div className='col-md-12'>
         {
-          contrib.map( (data:any) => {
-           return <div className='contributor-style'>
+          contrib.map((data: any) => {
+            return <div className='contributor-style' key={data.html_url}>
               <a href={data.html_url} target="_blank" rel="noreferrer">
-                <img src={data.avatar_url} alt=''/>
+                <img src={data.avatar_url} alt='' />
                 <span>{data.login}</span>
               </a>
             </div>
-          })          
+          })
         }
       </div>
     </>
   );
-  
+
 };
 export default Contributors;
