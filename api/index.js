6f56c200e8cd57c11626ba5de856bdaa69763253
David Augusto Taboada Dextre <davidxt48@hotmail.com>
2021-05-18 21:19:31 -0300

    update of connections

--- api/index.js
+++ api/index.js
@@ -60,14 +60,15 @@ export default async function (req,res) {
     let spore = find_token(tokens, {key:'symbol', value: 'spore'})
     let bscBurned = await new bsc.eth.Contract(abi_erc20, spore.bsc).methods.burned().call();
     let avaBurned = await new ava.eth.Contract(abi_erc20, spore.id).methods.balanceOf(spore.avaburn).call();
-    
+    let supplyavax = await new ava.eth.Contract(abi_erc20, spore.id).methods.totalSupply().call();
+    let supplybsc = await new bsc.eth.Contract(abi_erc20, spore.bsc).methods.totalSupply().call();
+
     let report = {
         bscBurned: bscBurned / 10 ** spore.decimals,
-        avaBurned: avaBurned / 10 ** spore.decimals
+        avaBurned: avaBurned / 10 ** spore.decimals,
+        supplyavax: supplyavax / 10 ** spore.decimals,
+        supplybsc: supplybsc / 10 ** spore.decimals
     };
-    
-    report.supplyavax = await new ava.eth.Contract(abi_erc20, spore.ava).methods.totalSupply().call();
-    report.supplybsc = await new bsc.eth.Contract(abi_erc20, spore.bsc).methods.totalSupply().call();
 
     report.circulatingSupply = spore.totalSupply - report.avaBurned - report.bscBurned; // - spore.totalFees / 2
     spore = Object.assign({}, spore, report);
