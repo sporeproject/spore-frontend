25a2b9494487df3cac80766c527b9e3d42bf0c7b
Alberto Florez <florez_alberto@hotmail.com>
2021-06-28 08:39:54 +0200

    change to tokenomics chart

--- src/components/BurnedTokens.tsx
+++ src/components/BurnedTokens.tsx
@@ -18,7 +18,7 @@ const BurnedTokens = ({
   totalTokenHoldersBSC,
 }: BurnedTokensInterfaceProps) => {
   
-
+  const TOTAL_SUPPLY= 100000000000000000
   const numberWithCommas = (x: number) => {
     return x.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
   }
@@ -35,10 +35,13 @@ const BurnedTokens = ({
         Total burned:<br></br> <b>{numberWithCommas(burnedTotal)}</b>
       </div>
       <div className="alert alert-dark" role="alert">
-        Avalanche holders: <br></br><b>{totalTokenHolders}</b>
+              Original Supply:<br></br>  <b>{numberWithCommas(TOTAL_SUPPLY)}</b>
       </div>
       <div className="alert alert-dark" role="alert">
-        BSC holders:<br></br> <b>{totalTokenHoldersBSC}+ <a className="bscLink" href="https://bscscan.com/token/0x33a3d962955a3862c8093d1273344719f03ca17c">BSCscan</a></b>
+        Avalanche holders: <br></br><b>{numberWithCommas(totalTokenHolders)}</b>
+      </div>
+      <div className="alert alert-dark" role="alert">
+        BSC holders:<br></br> <b>{numberWithCommas(totalTokenHoldersBSC)}+ <a className="bscLink" href="https://bscscan.com/token/0x33a3d962955a3862c8093d1273344719f03ca17c">BSCscan</a></b>
       </div>
     </>
   );
