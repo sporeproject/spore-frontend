a7d0b04c89e4c92a58b748deaff53ae8b648fe92
Diego Becerra <dabc2703@gmail.com>
2021-09-12 22:29:04 -0500

    feat: Separate HTML sections into components

--- src/components/Contributors/contributors.tsx
+++ src/components/Contributors/contributors.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import './contributors.scss';
+import './Contributors.scss';
 
 const Contributors = () => {
   const [contrib, setContributors] = useState([]);
@@ -24,21 +24,35 @@ const Contributors = () => {
   coreContributors = coreContributors.filter((data:any) => {return data !== null})
   
   return (
-    <>
-      <div className='col-md-12'>
-        {
-          coreContributors.map((data: any) => {
-            
-            return <div className='contributor-style' key={data.html_url}>
-              <a href={data.html_url} target="_blank" rel="noreferrer">
-                <img src={data.avatar_url} alt='' className="pr-1" loading="lazy" />
-                <span>{data.login}</span>
-              </a>
-            </div>
-        })
-        }
+    <section className='bg-white min-vh-50 pb-5'>
+      <div className='container information py-5'>
+        <div className='row py-4'>
+          <div className='col-md-12 text-center'>
+            <h2 className='feature'>Developers</h2>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-md-12'>
+            {
+              coreContributors.map((data: any) => {
+                
+                return <div className='contributor-style' key={data.html_url}>
+                  <a href={data.html_url} target="_blank" rel="noreferrer">
+                    <img src={data.avatar_url} alt='' className="pr-1" loading="lazy" />
+                    <span>{data.login}</span>
+                  </a>
+                </div>
+            })
+            }
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-md-5 text-left py-4'>
+            Complete list and legacy developers <a className="contribs" href="https://github.com/sporeproject/spore-frontend/graphs/contributors">here</a>
+          </div>
+        </div>
       </div>
-    </>
+    </section>
   );
 
 };
--- src/components/Information/Information.tsx
+++ src/components/Information/Information.tsx
@@ -1,8 +1,9 @@
-import Contributors from '../Contributors/contributors';
+import { Contracts } from '../Contracts/Contracts';
+import Contributors from '../Contributors/Contributors';
+import { Partnerships } from '../Partnerships/Partnerships';
 import PriceToken from '../PriceToken/PriceToken';
 import Tokenomics from '../Tokenomics/Tokenomics';
 import './Information.scss';
-import { PartnersWrapper } from './Information.style';
 
 const Information = () => {
   return (
@@ -212,130 +213,15 @@ const Information = () => {
         </div>
       </section>
 
-      <section className='bg-white min-vh-50 pb-5'>
-        <div className='container information py-5'>
-          <div className='row py-4'>
-            <div className='col-md-12 text-center'>
-              <h2 className='feature'>Developers</h2>
-            </div>
-          </div>
-          <div className='row'>
+      
             <Contributors />
-          </div>
-          <div className='row'>
-            <div className='col-md-5 text-left py-4'>
-              Complete list and legacy developers <a className="contribs" href="https://github.com/sporeproject/spore-frontend/graphs/contributors">here</a>
-            </div>
-          </div>
-        </div>
-      </section>
+          
 
-      <section className='bg-white-darker pb-6'>
-        <div className='container information py-5'>
-          <div className='row py-6'>
-            <div className='col-md-12 text-center'>
-              <h2 className='feature'>Partnerships</h2>
-            </div>
-          </div>
-          <PartnersWrapper className='row py-5'>
-            <div className='col-md-6 py-3 text-center'>
-              <a href="https://pangolin.exchange/">
-                <img src="https://pangolin.exchange/logo.svg" alt="Pangolin Exchange" />
-              </a>
-            </div>
+      <Partnerships />
 
-            <div className='col-md-6 py-3 text-center'>
-              <a href="https://gaj.finance">
-                <img src="/img/gaj.png" alt="Gaj Finance" />
-              </a>
-            </div>
-          </PartnersWrapper>
-        </div>
-      </section>
-
-      <div className='container information py-5'>
-        <div className='row py-5'>
-          <div className='col-md-12 text-center'>
-            <h2 className='feature pb-4'>Contracts</h2>
-            <div className="info">
-              <div className="row social-links">
-                <a
-                  href='https://cchain.explorer.avax.network/address/0x6e7f5C0b9f4432716bDd0a77a3601291b9D9e985/transactions'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='btn btn-primary mx-1 mb-2 zeros'
-                >
-                  Avalanche token address
-                </a>
-                <a
-                  href='https://bscscan.com/token/0x33a3d962955a3862c8093d1273344719f03ca17c'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='btn btn-primary mx-1 mb-2'
-                >
-                  BSC token address
-                </a>
-              </div>
-              <div className="row social-links">
-                <a
-                  href='https://cchain.explorer.avax.network/tx/0x7df1694004dd6e994d31f76c3978718e017fe6e6112482866051aca7ab90caa6/token-transfers'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='btn btn-primary mx-1 mb-2'
-                >
-                  2.5% of supply burnt tx
-                </a>
-                <a
-                  href='https://cchain.explorer.avax.network/tx/0xe3e92326e2993a270a2fdd44a7301e6adccb7dd1b40bcc4ed9ed88ec963a22ab/token-transfers'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='btn btn-primary mx-1 mb-2'
-                >
-                  LP-burnt tx
-                </a>
-              </div>
-              <div className="row social-links">
-                <a
-                  href='https://cchain.explorer.avax.network/address/0x88Dd784dFaaB1a7752d2CC81071Fcd12C1c4E1db/'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='btn btn-primary mx-1 mb-2'
-                >
-                  DEV wallet address
-                </a>
-                <a
-                  href='https://cchain.explorer.avax.network/tx/0x5fa10181e6c9841aa2226b5468e2b92f0268feaf178626472428e9839ab76982/internal-transactions'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='btn btn-primary mx-1 mb-2'
-                >
-                  Renounced ownership tx
-                </a>
-              </div>
-              <div className="row social-links">
-                <a
-                  href='https://cchain.explorer.avax.network/address/0xc2457F6Eb241C891EF74E02CCd50E5459c2E28Ea/contracts'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='btn btn-primary mx-1 mb-2'
-                >
-                  NFT V1 Address
-                </a>
-                <a
-                  href='https://cchain.explorer.avax.network/address/0x6Fe55D097FC9C1d08B64f4b1c94ac9453B1c9abB/contracts'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='btn btn-primary mx-1 mb-2'
-                >
-                  Multisig Wallet Contract
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Contracts />
     </>
   )
 }
 
-export default Information
+export default Information;
