1b602f584abe7ab428bec4f53dadc2919c2b0987
Alberto Florez <florez_alberto@hotmail.com>
2023-05-14 11:13:21 +0200

    another typescript fix

--- src/utils/bignumber.ts
+++ src/utils/bignumber.ts
@@ -179,7 +179,7 @@ export function handleSignificantDecimals(
   }
   let result = new BigNumber(`${value}`).toFixed(decimals)
   result = new BigNumber(`${result}`).toString()
-  if (typeof result == null){
+  if (typeof new BigNumber(`${result}`).dp() == null){
     return null
   }
   return new BigNumber(`${result}`).dp() <= 2
