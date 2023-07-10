65d5382b073f95cea5adb042f4bd0c133ad6d021
Alberto Florez <florez_alberto@hotmail.com>
2023-07-10 12:02:19 +0200

    implement 404 on non existent pages

---
+++ src/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound: React.FC = () => {
+  return (
+    <div>
+      <h1>404 - Not Found</h1>
+      <p>The page you're looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import smoothscroll from 'smoothscroll-polyfill';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import NotFound from './NotFound';
 smoothscroll.polyfill();
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
