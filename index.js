344234033e35ddc3697780249538bb5cbe908fcc
Ismael Cortegana <ismael14_16@hotmail.com>
2024-02-19 09:07:02 -0500

    change to api spore project and suppor .env

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8088;
 const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
+
 app.use(express.static('dist'));
 app.listen(port);
 
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/api', async (req, res) => {
+  const apiUrl = process.env.API_URL || 'https://frontend-api.sporeproject.org/api/';
   try {
-    const response = await fetch('https://cmc-api.sporeproject.org/');
-    return res.json(await response.json())
+    const query = req.query.q;
+    const response = await fetch(query ? `${apiUrl}?q=${query}` : apiUrl);
+    const contentType = response.headers.get('content-type');
+    if (contentType.includes('text/html')) {
+      const text = await response.text();
+      return res.send(text);
+    } else {
+      const data = await response.json();
+      return res.json(data);
+    }
   } catch (error) {
     return res.status(500).send(error);
   }
